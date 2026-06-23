c5d404d98b117841fd753fc454ef4031cfc75ebc
Inamsucode <144798584+Inamsucode@users.noreply.github.com>
2026-06-23 14:05:03 +0500

    tailwind css configure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/templates/**/*.html"],
+  content: [
+    "./app/templates/**/*.html",
+    "./app/templates/*.html",
+    "./app/templates/components/*.html",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        emerald: {
+          400: '#34d399',
+          500: '#10b981',
+          600: '#059669',
+        },
+        slate: {
+          800: '#1e293b',
+          900: '#0f172a',
+          950: '#020617',
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
